709033054155a86d8e59645e77da52f1d8b6d9e8
Aneesh Anil <aneesh@geektrust.in>
2024-06-13 13:59:03 +0530

    format changed. Has problem specific empty method declaration for the candidates to implement.

--- src/index.js
+++ src/index.js
@@ -1,28 +1,36 @@
+/**
+ * 
+ * @param {int[]} arr1 - a non-decreasing sequence of integers
+ * @param {int[]} arr2 - a non-decreasing sequence of integers
+ * @returns {int[]} the merged array
+ * 
+ * This function will be called by our system. 
+ * So fill in your code inside this function.
+ * 
+ */
+function mergeArrays(arr1, arr2) {
+    // WRITE YOUR CODE HERE....
+
+}
+
+
 
 /*
-* This is the main entry point for the program. It will parse the input for you.
-* You don't need to change this.
+* **************************************************************************************
+*                          DO NOT EDIT THE CODE BELOW.
+* ***************************************************************************************
+* This code is used to boostrap your solution to be checked for correctness by our system.
 */
 function main() {
     const input = process.argv.slice(2)
-    if (input.length === 0) {
-        throw new Error("No argument passed")
-    }
+    
     const arr1 = JSON.parse(input[0])
     const arr2 = JSON.parse(input[1])
-    handle(arr1, arr2)
+
+    const merged = mergeArrays(arr1, arr2)
+    console.log(merged)
+    // console.log(JSON.stringify(merged))
 }
 
-
-/*
- * Use this method to write your solution. 
- * arr1 - Integer array
- * arr2 - Integer array
- */
-function handle(arr1, arr2) {
-    console.log('arr1:', arr1)
-    console.log('arr2:', arr2)
-    //TODO: implement the logic to handle each input
-}
 
 main()
